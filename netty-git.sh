ca5477346ea5cf458667ef365856f2604f037680
young1lin <2550110827@qq.com>
2020-10-28 00:10:42 +0800

    netty update

--- netty-git.sh
+++ netty-git.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 git add .
+echo "-----------------------------"
 echo "add all ...."
+echo "-----------------------------"
 git commit -m "netty update"
+echo "-----------------------------"
 echo "commit info"
+echo "-----------------------------"
 git push
+echo "-----------------------------"
 echo "git push ok!"
+echo "-----------------------------"
