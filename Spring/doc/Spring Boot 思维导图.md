17c14844c4e82ea382fbe88f4c552ce673cb7b9f
young1lin <2550110827@qq.com>
2021-01-07 11:39:05 +0800

    Spring Boot update

---
+++ Spring/doc/Spring Boot 思维导图.md
@@ -0,0 +1,1 @@
+https://www.processon.com/mindmap/5ff64a5af346fb340dec7b59
---
+++ Spring/spring-in-action/src/main/java/me/young1lin/spring/boot/thinking/factories/MyFactories.java
@@ -0,0 +1,12 @@
+package me.young1lin.spring.boot.thinking.factories;
+
+/**
+ * @author <a href="mailto:young1lin0108@gmail.com">young1lin</a>
+ * @date 2021/1/7 上午8:41
+ * @version 1.0
+ */
+public interface MyFactories {
+
+
+
+}
