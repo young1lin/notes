bb653198cc3bba5ea610278846d4188b88c8a681
young1lin <2550110827@qq.com>
2021-05-30 19:26:58 +0800

    Other update

---
+++ Datastructure&Algorithm/xzg-algorithm/xzg-algorithm-week-01/src/main/java/me/young1lin/xzg/algorithm/week02/Calculator.java
@@ -0,0 +1,9 @@
+package me.young1lin.xzg.algorithm.week02;
+
+/**
+ * @author <a href="mailto:young1lin0108@gmail.com">young1lin</a>
+ * @since 2021/5/29 下午4:51
+ * @version 1.0
+ */
+public class Calculator {
+}
