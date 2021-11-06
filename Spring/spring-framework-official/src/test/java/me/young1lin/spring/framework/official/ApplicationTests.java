bd85e2fd2cc5028a2a1a56d6ecf5424dfdd3bf75
young1lin <2550110827@qq.com>
2021-11-06 23:05:49 +0800

    update

---
+++ Spring/spring-framework-official/src/test/java/me/young1lin/spring/framework/official/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.young1lin.spring.framework.official;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
